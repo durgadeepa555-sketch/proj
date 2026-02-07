8edcccadfafb2ab2f6caeb6af15417924fc1ba17
DURGADEEPA5 <ddpalaneeru5@gamil.com>
2026-02-07 12:41:47 +0530

    Added animation

---
+++ script.js
@@ -0,0 +1,40 @@
+function unlock() {
+  const code = document.getElementById("passcode").value;
+  const correctCode = "44319"; // CHANGE THIS ❤️
+
+  if (code === correctCode) {
+    document.getElementById("lockScreen").style.display = "none";
+    document.getElementById("content").style.display = "block";
+    document.getElementById("bgMusic").play();
+  } else {
+    alert("Wrong code 😛 Try again");
+  }
+}
+
+function valentine() {
+  document.getElementById("answer").innerText =
+    "Lol, I knew you’d click";
+}
+
+document.getElementById("valentineBtn").addEventListener("click", () => {
+  const answer = document.getElementById("answer");
+  answer.classList.add("show");
+
+  for (let i = 0; i < 10; i++) {
+    createHeart();
+  }
+});
+
+function createHeart() {
+  const heart = document.createElement("div");
+  heart.classList.add("heart");
+  heart.innerText = "💖";
+
+  heart.style.left = Math.random() * 100 + "%";
+
+  document.getElementById("hearts").appendChild(heart);
+
+  setTimeout(() => {
+    heart.remove();
+  }, 2500);
+}
